0474ffaa44fec57bfdcc5bc8bb0ba243b3316326
Kayla Baker <kaylambaker456@gmail.com>
2023-04-30 14:49:38 -0500

    delete account

--- client/src/pages/authenticaiton.js
+++ client/src/pages/authenticaiton.js
@@ -1,43 +1,48 @@
-import './../App.css';
-import starryBg  from './../imgs/starry_bg.png';
-import title  from './../imgs/title.png';
+import '../App.css';
+import starryBg  from '../imgs/starry_bg.png';
+import title  from '../imgs/title.png';
+import loginPlanet  from '../imgs/planet1.png';
+import Navbar from '../pages/NavBar';
+
 import { useState } from "react";
-import Navbar from './NavBar';
 
-function App() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+import { GoogleAuthProvider, signInWithPopup, reauthenticateWithCredential } from 'firebase/auth';
+import { auth } from '../firebase/firebaseConfig';
 
-  return (
+export default function Login() {
+  function DeleteUser() {
+      const user = auth.currentUser;
+   
+      user.delete()
+          .then(() => {
+              // User deleted.
+              console.log("User Account Deleted Successful");
+          })
+          .catch((error) => {
+          console.log(error)
+              // An error occurred
+              // ...
+          });
+  }
+  const handleGoogle = async (event) => {
+    const provider = await new GoogleAuthProvider();
+    return signInWithPopup(auth, provider);
+  }
+
+  return(
+    <div>
+      <Navbar/>
     <div style={{position: 'relative'}}>
-    <Navbar/>
       <img style={{height: '100%', width: '100%'}} src={starryBg} alt="starry background"/>
       <div style={{position: 'absolute', top: '10%', left: '30%', right: '0', bottom: 0, justifyContent: 'center', alignItems: 'center'}}>
-        <h1 style={{color: 'white'}}>Solar System Scribe</h1>
-
-      <div>
-          <form onSubmit={'#'}>
-            <div>
-              <label>
-                <input type="text" placeholder="email" onChange={(e) => setEmail(e.target.value)}/>
-              </label>
-            </div>
-            <div>
-              <label>
-                <input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} />
-              </label>
-            </div>
-            <button type="submit">
-              Submit
-            </button>
-          </form>
-          <a className="App-link" href="/signup">
-            <p style={{ fontSize: "12px" }}>create account</p>
-          </a>
+        <div>
+        <img src={title}/>
+        <div>
+          <button onClick={DeleteUser}>delete account </button>
         </div>
+      </div>
         </div>
     </div>
-  );
+    </div>
+  )  
 }
-
-export default App;
--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,43 +1,34 @@
-import './../App.css';
-import starryBg  from './../imgs/starry_bg.png';
-import title  from './../imgs/title.png';
+import '../App.css';
+import starryBg  from '../imgs/starry_bg.png';
+import title  from '../imgs/title.png';
+import loginPlanet  from '../imgs/planet1.png';
+import Navbar from '../pages/NavBar';
+
 import { useState } from "react";
-import Navbar from './NavBar';
 
-function App() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+import { GoogleAuthProvider, signInWithPopup, reauthenticateWithCredential } from 'firebase/auth';
+import { auth } from '../firebase/firebaseConfig';
 
-  return (
+export default function Login() {
+  const handleGoogle = async (event) => {
+    const provider = await new GoogleAuthProvider();
+    return signInWithPopup(auth, provider);
+  }
+
+  return(
+    <div>
+      <Navbar/>
     <div style={{position: 'relative'}}>
-    <Navbar/>
       <img style={{height: '100%', width: '100%'}} src={starryBg} alt="starry background"/>
       <div style={{position: 'absolute', top: '10%', left: '30%', right: '0', bottom: 0, justifyContent: 'center', alignItems: 'center'}}>
-        <h1 style={{color: 'white'}}>Solar System Scribe</h1>
-
-      <div>
-          <form onSubmit={'#'}>
-            <div>
-              <label>
-                <input type="text" placeholder="email" onChange={(e) => setEmail(e.target.value)}/>
-              </label>
-            </div>
-            <div>
-              <label>
-                <input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} />
-              </label>
-            </div>
-            <button type="submit">
-              Submit
-            </button>
-          </form>
-          <a className="App-link" href="/signup">
-            <p style={{ fontSize: "12px" }}>create account</p>
-          </a>
+        <div>
+        <img src={title}/>
+        <div>
+          <input type="image" src={loginPlanet} onClick={handleGoogle}/>
         </div>
+      </div>
         </div>
     </div>
-  );
+    </div>
+  )  
 }
-
-export default App;
